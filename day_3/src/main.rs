2241d3377ee00a534f5be103a2d99ccf42254191
Matheus Rogge <10780136+MatheusRogge@users.noreply.github.com>
2024-12-05 00:14:00 -0300

    feat: day_3 part two

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -3,17 +3,34 @@ use std::io::{self, Read};
 use regex::Regex;
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let regex = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)")?;
-
     let mut result = 0;
+    let mut enabled = true;
+
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
+    let regex = Regex::new(r"((do|don't)\(\))|(mul\(([0-9]{1,3}),([0-9]{1,3})\))")?;
     for captures in regex.captures_iter(input.as_str()) {
-        let left: i64 = captures.get(1).map(|e| e.as_str()).unwrap().parse()?;
-        let right: i64 = captures.get(2).map(|e| e.as_str()).unwrap().parse()?;
+        let enable_instruction = captures.get(2).map(|e| e.as_str());
+        match enable_instruction {
+            Some("do") => enabled = true,
+            Some("don't") => enabled = false,
+            _ => {},
+        }
 
-        result += left * right;
+        if !enabled {
+            continue;
+        }
+
+        let left_param = captures.get(4).map(|e| e.as_str());
+        let right_param = captures.get(5).map(|e| e.as_str());
+
+        if let (Some(left), Some(right)) = (left_param, right_param) {
+            let left: i64 = left.parse()?;
+            let right: i64 = right.parse()?;
+
+            result += left * right;
+        }
     }
 
     println!("Result: {:?}", result);
